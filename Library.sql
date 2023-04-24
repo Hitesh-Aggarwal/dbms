a4841ddb9400d66c2ed58ac776d79782a4c4d84e
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-04-24 16:39:53 +0530

    corrected minor error in er diagram and tables. also added some insert statements.

--- Library.sql
+++ Library.sql
@@ -14,7 +14,6 @@ CREATE TABLE
     bookname varchar(50),
     author varchar(40),
     publication varchar(20),
-    cost number,
     copies number,
     lost_cost number,
     delay_cost number
@@ -64,3 +63,18 @@ ALTER TABLE book ADD CONSTRAINT book_fk FOREIGN KEY (isbn) REFERENCES lib (isbn)
 ALTER TABLE subscription ADD CONSTRAINT subscription_fk_roll FOREIGN KEY (rollno) REFERENCES student (rollno);
 
 ALTER TABLE subscription ADD CONSTRAINT subscription_fk_book FOREIGN KEY (bookid) REFERENCES book (bookid);
+
+
+-- some insert statements
+
+-- student
+INSERT INTO student VALUES (1, 'ALFRED', 623623623, 0, 0);
+INSERT INTO student VALUES (2, 'JAMES', 659659659,0, 0);
+INSERT INTO student VALUES (3, 'GEORGE', 654654654, 0, 0);
+INSERT INTO student VALUES (4, 'TOM', 658658658,0, 0);
+INSERT INTO student VALUES (5, 'PETER', 652652652,0, 0);
+INSERT INTO student VALUES (6, 'JENNY', 651651651,0, 0);
+INSERT INTO student VALUES (7, 'ROSE', 657657657,0, 0);
+INSERT INTO student VALUES (8, 'MONICA', 639639639, 0, 0);
+INSERT INTO student VALUES (9, 'PHOEBE', 678678678, 0, 0);
+INSERT INTO student VALUES (10, 'RACHEL', 687687687,0, 0);
